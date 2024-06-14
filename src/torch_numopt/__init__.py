e0e01e2e03caf6162c0bec669a94af58dfba47d2
eugenioLR <eugeniolorente0@gmail.com>
2024-06-14 12:22:05 +0200

    added Gradient descent with line search

--- src/torch_numopt/__init__.py
+++ src/torch_numopt/__init__.py
@@ -3,3 +3,4 @@ from .newton_optim import NewtonRaphson
 from .gauss_newton_optim import GaussNewton
 from .lm_optim import LM
 from .agd_optim import AGD
+from .gradient_descent_lineseach import GradientDescentLS
